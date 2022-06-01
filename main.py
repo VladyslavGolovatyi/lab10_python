c5ea0ffbf3ba85895f9fa467bb3c088a2fd6678b
Владислав Головатий <93156003+VladyslavGolovatyi@users.noreply.github.com>
2022-06-01 18:55:47 +0300

    Add files via upload

---
+++ main.py
@@ -0,0 +1,10 @@
+from linked_list import *
+
+linked_list1 = LinkedList()
+linked_list1.insert(Node("TH-375", 50, 100, 95))
+linked_list1.insert(Node("AV-13", 0.1, 15, 99))
+linked_list1.insert(Node("TH-481", 10, 5, 50))
+linked_list1.insert(Node("HR-12", 0.05, 70, 99.9))
+linked_list1.insert(Node("BK-472", 10, 500, 40))
+linked_list1.print_info(60)
+
